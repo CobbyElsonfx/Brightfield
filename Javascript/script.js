99a40d936fdf15d07ec2db4d0b79218802f5954d
CobbyElsonfx <andohfrancis9187@gmail.com>
2022-10-05 22:52:24 -0700

    updated

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,3 +3,15 @@ toggler.onclick = function(){
     document.getElementByClassName("collapse")
 
 }
+
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
